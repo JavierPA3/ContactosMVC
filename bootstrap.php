d4d681023a834943983ac14462f9760d26d6ff34
JavierPA3 <91657168+JavierPA3@users.noreply.github.com>
2024-03-03 12:53:02 +0100

    Add files via upload

---
+++ bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+require 'vendor/autoload.php';
+use Dotenv\Dotenv;
+
+$dontenv = Dotenv::createImmutable(__DIR__);
+$dontenv->load();
+
+define('DBHOST', $_ENV['DBHOST']);
+define('DBNAME', $_ENV['DBNAME']);
+define('DBUSER', $_ENV['DBUSER']);
+define('DBPASS', $_ENV['DBPASS']);
+define('DBPORT', $_ENV['DBPORT']);
+
+define('APIPRIVADA', false);
